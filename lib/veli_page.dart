284fef2dba711d588166df1a38e27375fcd3cf01
hvmq1 <117809343+hvmq1@users.noreply.github.com>
2023-03-20 15:43:44 +0700

    add profile -HVMQ

--- lib/veli_page.dart
+++ lib/veli_page.dart
@@ -1,12 +1,13 @@
 import 'package:get/get.dart';
 import 'package:veli_flutter/veli_navigation.dart';
 import 'package:veli_flutter/view/description_view/description_view.dart';
+import 'package:veli_flutter/view/profile_view/views/profile_view.dart';
 
 class VeliPage {
   static final pages = [
     GetPage(
       name: VeliNavigation.descriptionView,
-      page: () => const DescriptionView(),
-    )
+      page: () => const ProfileView()),
+    
   ];
 }
